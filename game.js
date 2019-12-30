92e445fcd745d585cd673f8e0ee171a6654fa249
Christian Marc Caballero <mez.codedev@gmail.com>
2019-12-30 18:30:50 -0500

    Added the 'setTimeout' command.

--- game.js
+++ game.js
@@ -1,23 +1,28 @@
 //ROCK PAPER SCISSORS
-function gameStart(){
-    console.log('Welcome to ROCK PAPER SCISSORS!!!!!')
-}
-
 function playerInit(){
-    let playerPrompt = require('readline-sync');
-    let errorMsg = '';
-    for (let playerResponse = playerPrompt.question('How many players will be playing? ')){
-        if (playerResponse > 2){
-            errorMsg = 'Sorry, that player count is not yet available. Please input: 2. ';
-        }else if (playerResponse == 0){
-            errorMsg = "How can you play then... LOL. Let's ttry that again."
-        }else{
-            gameStart();
-        }
+    let playerPrompt = require(`readline-sync`);
+    let playerResponse = playerPrompt.question(`How many players will be playing? `);
+    let errorMsg = ``;
+
+    if (playerResponse > 2){
+        errorMsg = `Sorry. That player count is not yet available. Please input: 2`;
+        console.log(errorMsg);
+        playerInit();
+    }else if (playerResponse == 0){
+        errorMsg = `How do you even play then?... LOL! Let's try that again.`;
+        console.log(errorMsg);
+        playerInit();
+    }else{
+        gameStart();
     }
-}
+};
+
+function gameStart(){
+    console.log(`Welcome to ROCK PAPER SCISSORS!!!!!`);
+    setTimeout(()=>{console.log(`Let's begin!`)}, 2000);
+    console.log(`Please choose either: "ROCK, PAPER, or SCISSORS" by typing your selection below.`);
+};
+
+
+//function calls
 playerInit();
-
-// function gameStart(){tbgrvfrgryhrgtfetrgfe
-
-// }
